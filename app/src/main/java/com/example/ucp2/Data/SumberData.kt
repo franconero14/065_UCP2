e07d685668a0da60ebde2b7205f1792c7d47e8c5
franconero14 <nerobalor@gmail.com>
2023-11-17 15:18:46 +0700

    add string dosen pembimbing and add Sumber data for dosen pembimbing

---
+++ app/src/main/java/com/example/ucp2/Data/SumberData.kt
@@ -0,0 +1,11 @@
+package com.example.ucp2.Data
+
+import com.example.ucp2.R
+
+object SumberData {
+    val DosenPembimbing = listOf(
+        R.string.harissetiawan,
+        R.string.cahyadamarjati,
+        R.string.aprilkurniati,
+    )
+}
